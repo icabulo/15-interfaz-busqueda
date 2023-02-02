f851ab263f877af9e69192ad4a1b43b4c758d868
Ivan <icabulo@gmail.com>
2023-02-01 19:30:59 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const sendBtn = document.querySelector("form");
+const inputText = document.querySelector(".search-input");
+const showText = document.querySelector(".show-text");
+let userText = "";
+
+inputText.addEventListener("change", (e) => {
+  userText = e.target.value;
+});
+
+sendBtn.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (userText === "") {
+    showText.innerHTML = "";
+  }
+  showText.innerText = userText;
+  inputText.value = "";
+  userText = "";
+});
